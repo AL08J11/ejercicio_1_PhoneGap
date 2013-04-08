2907a58a1671f7c73871b25e24e6d73829fe43db
ALBERTO CHAPULIN LOPEZ <alberth.lop95@gmail.com>
2013-04-08 13:33:48 -0500

    version 1.0

---
+++ JS/acciones.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+$(document).ready(  function(e){
+	//alert("LISTO!!!")
+	document.addEventListener("deviceready",function(){
+		$('.btn').click(function(){
+		var boton=(($(this).attr('class')).split(' '))[1];
+		//alert(boton)
+		switch(boton)
+		{
+		case 'b1':
+		navigator.notification.beep(1);
+		break
+		case 'b2':
+		navigator.notification.beep(2);
+		break
+		case 'b3':
+		navigator.notification.beep(5);
+		break
+		case 'v1':
+		navigator.notification.vibrate(500);
+		break
+		case 'v2':
+		navigator.notification.vibrate(500);
+		navigator.notification.vibrate(1000);
+		break
+		}
+		});
+		$("derecha").swiperight(function(){
+			navigator.notification.alert("Desliz&oacute; a la derecha",function(){
+			},"Practica 1","Aceptar")
+		});
+	},false);
+});
